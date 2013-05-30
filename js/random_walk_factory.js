bf013cfa77dbacf22294727eca8fcf4d07f9691d
Richard Batty <rjlbatty@gmail.com>
2013-05-30 15:55:00 +0100

    randomWalkFactory uses stepGenerator to generate new population.

--- js/random_walk_factory.js
+++ js/random_walk_factory.js
@@ -2,9 +2,21 @@ function stepGenerator () {
   return Math.random() < 0.5;
 }
 
-function randomWalkFactory () {
+function randomWalkFactory (_stepGenerator) {
+  if (typeof _stepGenerator === "undefined") {
+    _stepGenerator = stepGenerator;
+  }
   return function (population) {
-    return population;
+    var delta = _stepGenerator() ? 1 : -1
+      , newPopulation
+      ;
+
+    newPopulation = [
+      population[0] + delta,
+      population[1] - delta
+    ];
+
+    return newPopulation;
   };
 }
 
--- test/random_walk_factory_spec.js
+++ test/random_walk_factory_spec.js
@@ -3,8 +3,19 @@ var path = require("path")
   , randomWalkFactory = require(path.resolve(__dirname, "../js/random_walk_factory.js"));
 
 describe('randomWalk', function() {
+
   beforeEach(function() {
     this.randomWalk = randomWalkFactory.randomWalkFactory();
+
+    this.stepGenerator = (function() {
+      var steps = [true, true, false, true]
+        , i = 0
+        ;
+
+      return function() {
+        return steps[i++];
+      };
+    })();
   });
 
   it('should be a function', function() {
@@ -15,18 +26,31 @@ describe('randomWalk', function() {
   describe('when given a population', function() {
     beforeEach(function() {
       this.initialPopulation = [5, 5];
-      this.randomWalk = randomWalkFactory.randomWalkFactory(this.initialPopulation);
+      this.randomWalk = randomWalkFactory.randomWalkFactory(this.stepGenerator);
     });
 
     it('should return a population of the same size', function() {
       var newPopulation = this.randomWalk(this.initialPopulation);
       newPopulation.length.should.eql(2);
     });
+
+    it('should return a population adjusted as expected', function() {
+      var populations = [[6,4], [7,3], [6,4], [7,3]]
+        , population = [5,5]
+        ;
+
+      for (var i = 0; i < 4; i++){
+        population = this.randomWalk(population);
+        population.should.eql(populations[i]);
+      }
+    });
   });
 
   describe('stepGenerator', function() {
     it('should return a boolean value', function() {
       randomWalkFactory.stepGenerator().should.be.a('boolean');
-    })
-  })
+    });
+  });
+
+
 });
